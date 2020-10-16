befd5fcb663c9ae1119ec8bd1ad6f671e0da0140
maxwen <max.weninger@gmail.com>
2020-10-16 16:50:45 +0200

    surfaceflinger: add property debug.sf.disable_hwc_overlays
    see development settings 'Disable HWC overlays'
    
    Change-Id: I494ba9c11006f893dc049a2d7aad6ed1c1fbc7a0

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -559,6 +559,9 @@ SurfaceFlinger::SurfaceFlinger(Factory& factory) : SurfaceFlinger(factory, SkipI
     property_get("debug.sf.disable_client_composition_cache", value, "0");
     mDisableClientCompositionCache = atoi(value);
 
+    property_get("debug.sf.disable_hwc_overlays", value, "0");
+    mDebugDisableHWC = atoi(value);
+
     char property[PROPERTY_VALUE_MAX] = {0};
     if((property_get("vendor.display.vsync_reliable_on_doze", property, "0") > 0) &&
         (!strncmp(property, "1", PROPERTY_VALUE_MAX ) ||
